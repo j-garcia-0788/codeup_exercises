6da4d9462eda5e1e1f31fb1d93242d58e603688e
James Garcia <james.m.garcia7@gmail.com>
2014-10-02 11:33:49 -0500

    first commit

---
+++ cars.php
@@ -0,0 +1,29 @@
+<?php
+
+$cars = [
+	
+	'Isaac' => [
+		'Make' => 'Toyota',
+		'Model' => 'Camry',
+		'year' => '2001'
+	],
+
+	'Liz' => [
+		'Make' => 'Ford',
+		'model' => 'Focus',
+		'year' => '2012'
+
+	],
+
+	'Margaret' => [
+		'make' => 'Nissan',
+		'model' => 'Sentra',
+		'year' => '2014'
+	]
+
+];
+
+echo 'Isaac car model is' . $cars['Isaac']['Model'] . "\n";
+
+
+?>
