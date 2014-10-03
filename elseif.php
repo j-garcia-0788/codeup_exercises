313827ca72fe823d20abe4c52645b890290cc4e5
James Garcia <james.m.garcia7@gmail.com>
2014-10-03 13:24:03 -0500

    else if

---
+++ elseif.php
@@ -0,0 +1,31 @@
+<?php
+
+$a = 5; 
+$b = 10; 
+$c = '10';
+
+if ($a < $b) {
+    echo "$a is less than $b\n";
+
+} else {
+    echo "$b is greater than $a\n";
+}
+
+if ($b >= $c) {
+    echo "$b is less than or equal to $c\n";
+
+} else {
+    echo "$b is less than or equal to $c\n";
+}
+
+if ($b == $c) {
+    echo "$b is equal to $c\n";
+} elseif ($b === $c) {
+    echo "$b is identical to $c\n";
+} elseif ($b != $c) {
+    echo "$b is not equal to $c\n";
+} elseif ($b !== $c) {
+    echo "$b is not identical to $c\n";
+}
+
+?>
