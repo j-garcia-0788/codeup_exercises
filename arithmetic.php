fb450ca0d20be6ee07918cf0ee66d4ec5387bcfa
James Garcia <james.m.garcia7@gmail.com>
2014-10-08 09:30:47 -0500

    arithmetic

---
+++ arithmetic.php
@@ -0,0 +1,31 @@
+<?php
+
+function add($a, $b) {
+	echo $a + $b . PHP_EOL; 
+}
+
+add (3, 3);
+
+function subtract($a, $b) {
+	echo $a - $b . PHP_EOL;
+} 
+
+subtract(3, 3);
+
+function multiply($a, $b) {
+	echo $a * $b . PHP_EOL; 
+}
+
+multiply(3, 3);
+
+function divide($a, $b) {
+	echo $a / $b . PHP_EOL;
+}
+
+divide(3, 3);
+
+function modulus($a, $b) {
+	echo $a % $b . PHP_EOL;
+}
+
+modulus(5, 8);
